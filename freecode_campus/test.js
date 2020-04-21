e5f37a21dd818ecbb703f1052cf194990439bb1c
xiaokang-chen <346626210@qq.com>
2020-04-21 23:50:26 +0800

    Js部门看完、ES6看5小节、一道算法题

--- freecode_campus/test.js
+++ freecode_campus/test.js
@@ -17,4 +17,32 @@ function multiplesOf3and5(number) {
   return sum;
 }
 
-console.log(multiplesOf3and5(49));
+// console.log(multiplesOf3and5(49));
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val){
+    case 1:
+    case 2:
+    case 3:
+        answer = "Low"; 
+        break;
+    case 4:
+    case 5:
+    case 6:
+        answer = "Mid"; 
+        break;
+    case 7:
+    case 8:
+    case 9:
+        answer = "High"; 
+        break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+console.log(sequentialSizes(1));
